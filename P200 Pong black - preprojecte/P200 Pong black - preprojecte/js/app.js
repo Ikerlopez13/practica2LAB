963099cca71099380582b69fb4665c0b668217ab
OscarPujol15 <opujolr@edu.tecnocampus.cat>
2025-05-17 10:56:57 +0200

    oscar

---
+++ P200 Pong black - preprojecte/P200 Pong black - preprojecte/js/app.js
@@ -0,0 +1,29 @@
+//Variables i constants globals
+//Main de l'aplicatiu
+var joc;
+$(function(){
+
+    let myCanvas = $("#joc")[0];
+    let myCtx = myCanvas.getContext("2d");
+
+    /********************************* 
+     * Tasca. Inicialitza la classe JOC les posicions 
+     * dels elements del joc
+     * al canva: Pales, bola, etc
+    **********************************/  
+    
+    joc = new Joc(myCanvas, myCtx);
+    joc.inicialitza();
+    animacio();
+
+})
+
+function animacio(){
+    joc.update();
+    //Oportunitat per actualitzar les puntuacions
+    //revisar si seguim jugant o no
+    //Si pujem de nivell, etc
+
+    //Crida recursiva per generar animació
+    requestAnimationFrame(animacio);
+}
